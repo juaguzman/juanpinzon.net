a8586d838d90082a40a9bc20d4338e34c0e7e609
juaguzman <juancarlos_891111@hotmail.com>
2015-12-22 20:56:49 -0500

    principio

---
+++ gallery_1.php
@@ -0,0 +1,60 @@
+<?php
+
+class gallery 
+{
+   var $files = array();
+   var $path;
+   
+   function loadFolder($path){
+      $this->path = $path;
+      
+      //---Guardar en un arreglo todos los archivos en el directorio   
+      $folder = opendir($this->path);
+         
+      while ($fil = readdir($folder)) {
+         
+         //---Si no es un directorio
+         if(!is_dir($fil)){
+            
+            $arr = explode('.', $fil);
+            if(count($arr) > 1){
+               
+               //---Ir guardando los nombres en un arreglo
+               $this->files[] = $fil;
+            }
+         }
+      }
+      
+      //---Cerrar el directorio
+      closedir($folder);
+      
+      //---Ordenar alfabeticamente el arreglo
+      sort($this->files);
+   }
+
+   function show($area = '100%', $width = 1000, $space = 10){
+   
+      //---Crear la galería con los nombres de todos los archivos
+      $total = count($this->files);
+      $cont = 0;
+      
+      echo '<div name="xx" style="width:'.$area.'px">';
+      
+         //---Situar los thumbnails
+         for($i = 0; $i < $total; $i++){      
+           
+            echo '<a class="example-image-link" href="'.$this->path.'/'.$this->files[$i].'" data-lightbox="example-set" data-title="Haga clic en la mitad derecha de la imagen para avanzar, o mitad izquierda para retroseder"><img src='.$this->path.'/'.$this->files[$i].' width='.$width.' height='.$width.'/></a>';
+         }
+?>        
+                <script language="javascript">
+                $(document).ready(function(){
+                   $("a[rel = 'lightbox']").lightBox();
+                });
+                </script>        
+<?
+
+<?php
+      echo '</div>';
+   }
+}
+?>
--- index.php
+++ index.php
@@ -8,6 +8,7 @@ and open the template in the editor.
     <head>
         <meta charset="UTF-8">
         <link href="styles/style.css" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="styles/lightbox.css">
         <title></title>
     </head>
     
@@ -18,21 +19,24 @@ and open the template in the editor.
       </span>
     </header>
         <div>
-            buenas noches nueva yorck
+            <h1> GAleria</h1>
+           
         </div>
         
         <div align="center">    
 <?php      
       // Incluir la clase   
-      include_once('gallery.php');
+      include_once('./gallery_1.php');
       
       $mygallery = new gallery(); // Crear una nueva instancia
       $mygallery->loadFolder('capturas'); // Leer las imágenes de la carpeta "galley_images"
-      $mygallery->show('100%', 500, 10); // Mostrar la galería en este lugar en un area de 500px      
+      $mygallery->show('100%', 200, 0); // Mostrar la galería en este lugar en un area de 500px      
 ?>    
     </div>
         <footer>
             
         </footer>
+         <script src="js/lightbox-plus-jquery.js"></script>
     </body>
+   
 </html>
